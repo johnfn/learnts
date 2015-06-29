193b43e8151b2f67ae667aaf3e77cccd82d987b8
Grant Mathews <grant.mathews@sqor.com>
2015-06-29 15:11:36 -0700

    Start adding navigation

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
+/*
+  This was an experiment to see how it would be to write JavaScript/TypeScript without jQuery.
+
+  It was a good learning experiment, but I wouldn't recommend anyone to actually code this way. (It's actually
+  pretty nice that TypeScript can recognize that, e.g., getElementsByTagName("div") returns an array of HTMLDivElement
+  rather than just an array of HTMLElement or worse - but wow, stuff gets verbose quickly.)
+*/
 var start = function () {
     highlightCode();
+    buildTableOfContents();
     addListeners();
+};
+var buildTableOfContents = function () {
+    var headings = Array.prototype.slice.call(document.querySelectorAll("h2,h3,h4,h5,h6"));
+    var toc = document.getElementsByTagName("nav").item(0).getElementsByTagName("div").item(0);
+    for (var _i = 0; _i < headings.length; _i++) {
+        var heading = headings[_i];
+        var tocElement = document.createElement("div");
+        tocElement.innerHTML = heading.innerText;
+        toc.appendChild(tocElement);
+    }
 };
 var highlightCode = function () {
     var preNodes = Array.prototype.slice.call(document.getElementsByTagName("pre"));
@@ -19,12 +37,13 @@ var addListeners = function () {
             var asideNodes = Array.prototype.slice.call(document.getElementsByTagName("aside"));
             for (var _i = 0; _i < asideNodes.length; _i++) {
                 var node = asideNodes[_i];
-                if (node.dataset.hidden) {
+                if (node.dataset['hidden']) {
                     node.style.display = "none";
                 }
-                if (node.dataset.associatedid == id) {
+                if (node.dataset['associatedid'] == id) {
                     node.style.display = "block";
                     node.addEventListener("mouseover", function (ev) {
+                        // TODO
                     });
                 }
             }
--- main.ts
+++ main.ts
@@ -1,12 +1,33 @@
 declare var hljs: any;
 
+/*
+  This was an experiment to see how it would be to write JavaScript/TypeScript without jQuery.
+
+  It was a good learning experiment, but I wouldn't recommend anyone to actually code this way. (It's actually
+  pretty nice that TypeScript can recognize that, e.g., getElementsByTagName("div") returns an array of HTMLDivElement
+  rather than just an array of HTMLElement or worse - but wow, stuff gets verbose quickly.)
+*/
+
 var start = function() {
   highlightCode();
+  buildTableOfContents();
   addListeners();
 };
 
+var buildTableOfContents = function() {
+  var headings: HTMLHeadingElement[] = Array.prototype.slice.call(document.querySelectorAll("h2,h3,h4,h5,h6"));
+  var toc: HTMLDivElement = document.getElementsByTagName("nav").item(0).getElementsByTagName("div").item(0);
+
+  for (var heading of headings) {
+    var tocElement: HTMLDivElement = document.createElement("div");
+
+    tocElement.innerHTML = heading.innerText;
+    toc.appendChild(tocElement);
+  }
+};
+
 var highlightCode = function() {
-  var preNodes = Array.prototype.slice.call(document.getElementsByTagName("pre"));
+  var preNodes: HTMLPreElement[] = Array.prototype.slice.call(document.getElementsByTagName("pre"));
 
   for (var node of preNodes) {
     node.innerHTML = node.innerHTML.trim();
@@ -16,24 +37,24 @@ var highlightCode = function() {
 };
 
 var addListeners = function() {
-  var annotationNodes: Node[] = Array.prototype.slice.call(document.getElementsByClassName("code-annotation"));
+  var annotationNodes: HTMLAnchorElement[] = Array.prototype.slice.call(document.getElementsByClassName("code-annotation"));
 
   for (var node of annotationNodes) {
     node.addEventListener("click", (e: MouseEvent) => {
-      var id = (<any> e.currentTarget).id;
+      var id = (<HTMLAnchorElement> e.currentTarget).id;
 
-      var asideNodes: any[] = Array.prototype.slice.call(document.getElementsByTagName("aside"));
+      var asideNodes: HTMLElement[] = Array.prototype.slice.call(document.getElementsByTagName("aside"));
 
       for (var node of asideNodes) {
-        if (node.dataset.hidden) {
+        if (node.dataset['hidden']) {
           node.style.display = "none";
         }
 
-        if (node.dataset.associatedid == id) {
+        if (node.dataset['associatedid'] == id) {
           node.style.display = "block";
 
           node.addEventListener("mouseover", (ev: MouseEvent) => {
-
+            // TODO
           });
         }
       }
